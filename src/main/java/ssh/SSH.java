7fd8b3b55993a9ad947d5f0c0e41c780cdca7634
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-06-02 23:52:52 +0200

    Small example shows how library sshj works, added Builder to class SSH

--- src/main/java/ssh/SSH.java
+++ src/main/java/ssh/SSH.java
@@ -1,16 +1,36 @@
 package ssh;
 
-import javax.swing.tree.TreeModel;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import properties.Properties;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class SSH
 {
-	public static SSH connection;
+	public static SSH connection = new SSH();
+
+	private SSHClient sshClient;
 
 	private SSH()
 	{
+		SSHClient ssh = new SSHClient();
+		try
+		{
+			ssh.loadKnownHosts();
+			ssh.addHostKeyVerifier("3c:7e:e2:dc:2d:c1:f2:30:6a:81:4d:b6:63:eb:82:a3");
+			ssh.connect(Properties.values.get("ip-address").toString(), 22);
+			ssh.authPassword(Properties.values.get("username").toString(), Properties.values.get("password").toString());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 
 	}
-
 	public void uploadFile(String filepath, String destination)
 	{
 
@@ -21,9 +41,55 @@ public class SSH
 
 	}
 
-	public TreeModel getDirectories(String root)
+	public DefaultMutableTreeNode getDirectories(String root)
 	{
-		return null;
+		DefaultMutableTreeNode top = new DefaultMutableTreeNode("Hola");
+
+		return top;
+	}
+
+	public void closeClient()
+	{
+		try
+		{
+			this.sshClient.disconnect();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args)
+			throws IOException
+	{
+		final SSHClient ssh = new SSHClient();
+		ssh.loadKnownHosts();
+		ssh.addHostKeyVerifier("3c:7e:e2:dc:2d:c1:f2:30:6a:81:4d:b6:63:eb:82:a3");
+		ssh.connect(Properties.values.get("ip-address").toString(), 22);
+		Session session = null;
+		try
+		{
+			ssh.authPassword(Properties.values.get("username").toString(), Properties.values.get("password").toString());
+			session = ssh.startSession();
+			final Session.Command cmd = session.exec("ls -a");
+			System.out.println(IOUtils.readFully(cmd.getInputStream()).toString());
+			cmd.join(5, TimeUnit.SECONDS);
+		} finally
+		{
+			try
+			{
+				if (session != null)
+				{
+					session.close();
+				}
+			} catch (IOException e)
+			{
+				// Do Nothing
+			}
+
+			ssh.disconnect();
+		}
 	}
 
 }
