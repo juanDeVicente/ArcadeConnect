cd6a1abd899c5adaf2211c84929e195a5533bd54
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-06-03 23:02:59 +0200

    SSH now Upload content and get directory tree from root

--- src/main/java/ssh/SSH.java
+++ src/main/java/ssh/SSH.java
@@ -2,24 +2,31 @@ package ssh;
 
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.ConnectionException;
 import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.TransportException;
+import net.schmizz.sshj.xfer.FileSystemFile;
 import properties.Properties;
+import util.Util;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class SSH
 {
 	public static SSH connection = new SSH();
 
+
 	private SSHClient sshClient;
 
 	private SSH()
 	{
-		this.sshClient = new SSHClient();
 		try
 		{
+			this.sshClient = new SSHClient();
 			this.sshClient.loadKnownHosts();
 			this.sshClient.addHostKeyVerifier("3c:7e:e2:dc:2d:c1:f2:30:6a:81:4d:b6:63:eb:82:a3");
 			this.sshClient.connect(Properties.values.get("ip-address").toString(), 22);
@@ -28,35 +35,86 @@ public class SSH
 		catch (IOException e)
 		{
 			e.printStackTrace();
+			this.sshClient = null;
 		}
 
 	}
 	public void uploadFile(String filepath, String destination)
 	{
+		try (SFTPClient sftp = sshClient.newSFTPClient())
+		{
+			sftp.put(new FileSystemFile(filepath), destination);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void removeFile(String filepath)
+	{
 
 	}
 
-	public void removeFile(String filepath, String origin)
+	public void restartArcade()
 	{
-
+		try (Session session = sshClient.startSession())
+		{
+			Session.Command cmd = session.exec("sudo shutdown -r now");
+		}
+		catch (ConnectionException e)
+		{
+			e.printStackTrace();
+		}
+		catch (TransportException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	public DefaultMutableTreeNode getDirectories(String root)
 	{
-		String[] split = root.split("/");
-		String folderName = split[split.length - 1];
-		DefaultMutableTreeNode top = new DefaultMutableTreeNode(folderName.toLowerCase());
+		if (this.sshClient != null)
+			return createDirectoryTree(root);
 
-		try(Session session = sshClient.startSession())
+		return null;
+	}
+
+	public void closeClient()
+	{
+		if (this.sshClient != null)
 		{
-			Session.Command cmd = session.exec("ls -a " + root);
-			String[] data = IOUtils.readFully(cmd.getInputStream()).toString().split("\n");
-
-			int i = 0;
-			for (String d :data)
+			try
 			{
-				if (!d.equals(".") && !d.equals(".."))
-					top.add(new DefaultMutableTreeNode(d.toLowerCase()));
+				this.sshClient.disconnect();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private DefaultMutableTreeNode createDirectoryTree(String currentDir)
+	{
+		String[] split = currentDir.split("/");
+		String name = split[split.length - 1].toLowerCase();
+		DefaultMutableTreeNode node = new DefaultMutableTreeNode(name);
+
+		if (!Util.getFileExtension(name).equals(""))
+			return node;
+
+		try (Session session = sshClient.startSession())
+		{
+			Session.Command cmd = session.exec("ls -a " + currentDir);
+			String[] data = IOUtils.readFully(cmd.getInputStream()).toString().split("\n");
+			for (String d: data)
+			{
+				if (!d.equals(".") && !d.equals("..") && !d.equals(""))
+					node.add(createDirectoryTree(currentDir + "/" + d));
 			}
 		}
 		catch (IOException e)
@@ -64,19 +122,7 @@ public class SSH
 			e.printStackTrace();
 		}
 
-		return top;
-	}
-
-	public void closeClient()
-	{
-		try
-		{
-			this.sshClient.disconnect();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+		return node;
 	}
 
 	public static void main(String[] args)
@@ -104,7 +150,7 @@ public class SSH
 				}
 			} catch (IOException e)
 			{
-				// Do Nothing
+				e.printStackTrace();
 			}
 
 			ssh.disconnect();
